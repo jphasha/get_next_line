395dd71849a73c9266ce26beaa0bd6e7d8ad0f27
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-06-27 16:21:33 +0200

    get_next_line.h small update

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,17 @@
 /*   By: jphasha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 09:53:15 by jphasha           #+#    #+#             */
-/*   Updated: 2019/06/25 09:56:54 by jphasha          ###   ########.fr       */
+/*   Updated: 2019/06/27 16:20:35 by jphasha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#include "libft/libft.h"
+
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
 
 #endif
